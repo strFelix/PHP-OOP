4cfb19e627dd3ab7d746e7f8debccfc8ea9521ed
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2024-06-05 01:28:09 -0300

    feat: add minimal slug utilization

--- Composer/index.php
+++ Composer/index.php
@@ -1,7 +1,13 @@
 <?php
 
-echo "Hello World!";
-
 //composer require name/package -> add package
 //composer update -> update all packages
 //composer remove name/package -> remove package
+//composer install -> install all packages on composer.json
+
+require_once "vendor/autoload.php";
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify(); // slug is a string that is safe to use in URLs
+$slugify->addRule("World", "Earth"); // hello-earth
+echo $slugify->slugify("Hello World!", "_"); // hello-world
